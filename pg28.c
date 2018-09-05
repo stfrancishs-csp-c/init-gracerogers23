30c386b016a3b751b7d426340d1ffa3b92109e74
Catherine (Grace) Rogers <19rogerscat@stfrancishs.org>
2018-09-05 19:47:32 +0000

    init

---
+++ pg28.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main ()
+{
+    float a, b;
+    printf("How much of a bonus did you get?");
+    scanf("%f", &a);
+    b=.85
+    printf ("If you give 15 percent to charity, you will still ");
+    printf("have %.2f.",b);
+    return 0;
+}
